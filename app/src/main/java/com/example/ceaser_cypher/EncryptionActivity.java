5df1f7604a6c9eac8dac1e6152c0a92af2e9088d
Princeyyyy <princemumo80@gmail.com>
2021-03-26 19:46:52 +0300

    added encryption and decryption activity

---
+++ app/src/main/java/com/example/ceaser_cypher/EncryptionActivity.java
@@ -0,0 +1,14 @@
+package com.example.ceaser_cypher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EncryptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encryption);
+    }
+}
--- app/src/main/java/com/example/ceaser_cypher/MainActivity.java
+++ app/src/main/java/com/example/ceaser_cypher/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.ceaser_cypher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.encryption) Button encryptionButton;
+    @BindView(R.id.decryption) Button decryptionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        encryptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent encryptionintent = new Intent(MainActivity.this,EncryptionActivity.class);
+                startActivity(encryptionintent);
+            }
+        });
+
+        decryptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent decryptionintent = new Intent(MainActivity.this,DecryptionActivity.class);
+                startActivity(decryptionintent);
+            }
+        });
     }
 }
