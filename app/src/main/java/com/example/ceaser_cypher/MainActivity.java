3d72e1807dbf70c403133c8438da51f0ec024b9a
Princeyyyy <princemumo80@gmail.com>
2021-04-17 22:47:08 +0300

    added new animation and added slpash screen activity

--- app/src/main/java/com/example/ceaser_cypher/MainActivity.java
+++ app/src/main/java/com/example/ceaser_cypher/MainActivity.java
@@ -13,8 +13,10 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.encryption) Button encryptionButton;
-    @BindView(R.id.decryption) Button decryptionButton;
+    @BindView(R.id.encryption)
+    Button encryptionButton;
+    @BindView(R.id.decryption)
+    Button decryptionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         encryptionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent encryptionintent = new Intent(MainActivity.this,EncryptionActivity.class);
+                Intent encryptionintent = new Intent(MainActivity.this, EncryptionActivity.class);
                 startActivity(encryptionintent);
             }
         });
@@ -34,9 +36,21 @@ public class MainActivity extends AppCompatActivity {
         decryptionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent decryptionintent = new Intent(MainActivity.this,DecryptionActivity.class);
+                Intent decryptionintent = new Intent(MainActivity.this, DecryptionActivity.class);
                 startActivity(decryptionintent);
             }
         });
+    }
+
+    public void Encrypt(View view) {
+        Intent intent = new Intent(this, EncryptionActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+
+    public void Decrypt(View view) {
+        Intent intent = new Intent(this, DecryptionActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right);
     }
 }
