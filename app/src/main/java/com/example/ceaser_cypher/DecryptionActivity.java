4313523583ed5080209363ed11905c0e262b5139
Princeyyyy <princemumo80@gmail.com>
2021-04-17 14:56:14 +0300

    finished encryption

--- app/src/main/java/com/example/ceaser_cypher/DecryptionActivity.java
+++ app/src/main/java/com/example/ceaser_cypher/DecryptionActivity.java
@@ -3,12 +3,27 @@ package com.example.ceaser_cypher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DecryptionActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.decryptiontext)
+    EditText decryptionText;
+    @BindView(R.id.decryptionbutton)
+    Button decryptionbutton;
+    @BindView(R.id.encrypted)
+    TextView encrypted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decryption);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/example/ceaser_cypher/EncryptionActivity.java
+++ app/src/main/java/com/example/ceaser_cypher/EncryptionActivity.java
@@ -3,12 +3,61 @@ package com.example.ceaser_cypher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EncryptionActivity extends AppCompatActivity {
+
+    @BindView(R.id.encryptiontext)
+    EditText encryptionText;
+    @BindView(R.id.encryptionbutton)
+    Button encryptionButton;
+    @BindView(R.id.encrypted)
+    TextView encrypted;
+//    @BindView(R.id.shiftkey)
+//    EditText shiftKey;
+
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encryption);
+        ButterKnife.bind(this);
+
+        encryptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Editable x = encryptionText.getText();
+                encrypted.setText(EncrytionCc(x));
+            }
+        });
+    }
+
+//    EditText shiftKey = findViewById(R.id.shiftkey);
+//    String value = shiftKey.getText().toString();
+//    int finalValue = Integer.parseInt(value);
+
+    int key = 5;
+
+    String EncrytionCc(Editable plain) {
+        String temp = "";
+        for (int i = 0; i < plain.length(); i++) {
+
+            if (plain.charAt(i) == ' ') {
+                temp += ' ';
+            } else if (plain.charAt(i) + key > 'z') {
+                temp += ((char) (plain.charAt(i) - (26 - key)));
+            } else
+                temp += ((char) (plain.charAt(i) + key));
+        }
+        return temp;
+
     }
 }
