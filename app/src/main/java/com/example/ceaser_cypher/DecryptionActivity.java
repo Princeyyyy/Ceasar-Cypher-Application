6c1549b8ab539c66195abfff90ff857326d7efa0
Princeyyyy <princemumo80@gmail.com>
2021-04-17 15:02:21 +0300

    finished decryption

--- app/src/main/java/com/example/ceaser_cypher/DecryptionActivity.java
+++ app/src/main/java/com/example/ceaser_cypher/DecryptionActivity.java
@@ -3,6 +3,8 @@ package com.example.ceaser_cypher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,13 +19,38 @@ public class DecryptionActivity extends AppCompatActivity {
     EditText decryptionText;
     @BindView(R.id.decryptionbutton)
     Button decryptionbutton;
-    @BindView(R.id.encrypted)
-    TextView encrypted;
+    @BindView(R.id.decrypted)
+    TextView decrypted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decryption);
         ButterKnife.bind(this);
+
+        decryptionbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Editable y = decryptionText.getText();
+                decrypted.setText(DecrytionCc(y));
+            }
+        });
+    }
+
+    int key = 5;
+
+    String DecrytionCc(Editable cipher) {
+        String temp = "";
+        for (int i = 0; i < cipher.length(); i++) {
+
+            if (cipher.charAt(i) == ' ') {
+                temp += ' ';
+            } else if (cipher.charAt(i) - key < 'a') {
+                temp += ((char) (cipher.charAt(i) - (key - 26)));
+            } else
+                temp += ((char) (cipher.charAt(i) - key));
+        }
+        return temp;
+
     }
 }
